6b063cc2db40ca8730ab867efdc7e470a311b25d
Elena Porras <m.elenaporras@hotmail.com>
2017-09-29 06:17:30 -0600

    added navbars to the pages

--- echo.php
+++ echo.php
@@ -19,7 +19,21 @@
     <title>Mobiles</title>
 </head>
 <body>
-   
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+            <a class="nav-link" href="adminform.php">Add a mobile </a>
+        </li>
+        <li class="nav-item active">
+            <a class="nav-link" href="echo.php">Mobile list</a><span class="sr-only">(current)</span>
+        </li>
+        </ul>
+    </div>
+    </nav>
 
 <div class="container">
 
