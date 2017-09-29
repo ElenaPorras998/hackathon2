9f7183750319aa2afe08e5bf84a2621b2e3b57cc
LuisVuitton <holbikl@gmail.com>
2017-09-29 11:15:47 +0200

    added adminer file and small changes

--- echo.php
+++ echo.php
@@ -1,30 +1,32 @@
 <?php 
-require 'genres.php';
+require 'os.php';
+require 'colors.php';
 require_once 'db.php';
 $db = db_connect();
 
-$stmt = $db->prepare('SELECT * FROM movies');
+$stmt = $db->prepare('SELECT * FROM mobiles');
 $stmt ->execute();
 $mobiles = $stmt->fetchAll();
 
 foreach ($mobiles as $mobile) {
-    echo 'Movie: ' . htmlspecialchars($mobile['brand']);
+    echo 'Brand: ' . htmlspecialchars($mobile['brand']);
     echo '<br>';
     echo '<br>';
-    echo 'Description: ' . htmlspecialchars($mobile['model']);
+    echo 'Model: ' . htmlspecialchars($mobile['model']);
     echo '<br>';
     echo '<br>';
-    echo 'Director: ' . htmlspecialchars($mobile['price']);
+    echo 'Price: ' . htmlspecialchars($mobile['price']);
     echo '<br>';
     echo '<br>';
-    echo 'genre: ' . htmlspecialchars($os[$mobile['os']]);
+    echo 'OS: ' . htmlspecialchars($os[$mobile['os']]);
     echo '<br>';
-    echo 'genre: ' . htmlspecialchars($os[$mobile['color']]);
+    echo 'Color: ' . htmlspecialchars($os[$mobile['color']]);
     echo '<br>';
     echo  '<a href="edit.php?id=' . htmlspecialchars($mobile['id']) . '">edit</a>';
     echo '<br>';
     echo '<br>';
     echo '<br>';
 }
+
 
 ?>
